214e36defe6caff5b9cc46bbfa29308ebd48379c
Valentina Roxo <roxovalentina@gmail.com>
2025-09-05 10:31:25 -0300

    Versão módulo http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -3,3 +3,4 @@ http.createServer((req, res) => { //criando um servidor
   res.writeHead(200, {'Content-Type': 'text/plain'}); //definindo o cabeçalho da resposta
   res.end('Hello World!'); //finalizando a resposta com uma mensagem
 }).listen(8080); //servidor ouvindo na porta 8080 
+console.log('Servidor rodando em http://localhost:8080/');
---
+++ nodejs/modulos/http/cliente.js
@@ -0,0 +1,18 @@
+//importando o módulo http
+const http = require('http');
+
+http.get("http://jsonplaceholder.typicode.com/todos/1", (res) => {
+  let data = '';
+
+  //Um pedaço de dados foi recebido.
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  //A resposta inteira foi recebida. Imprime o resultado.
+  res.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
